f22ff9e95c3daed45d9219f8a04c0c024c7029bf
Anurag Thakur <anuragthakur2102@gmail.com>
2025-05-15 06:02:40 +0530

    Basic interaction with bot using commands

---
+++ commands.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+  {
+    name: 'create',
+    description: 'Creates a new  short url',
+    options: [
+      {
+        name: 'url',
+        type: 3, // STRING
+        description: 'The URL to shorten',
+        required: true,
+      },
+    ],
+  }
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
--- index.js
+++ index.js
@@ -11,7 +11,50 @@ const client = new Client({
 });
 
 client.on('messageCreate', (message) => {
-    console.log(message.content);
+    // console.log(message.content);
+    if(message.author.bot) return; //ignore bot messages
+    if(message.content === 'ping') {
+        message.reply(
+            {content: "Pong!"}
+        );
+    }
+    if(message.content === 'hello') {
+        message.reply(
+            {content: "Hello!"}
+        );
+    }
+    if(message.content === 'hi') {
+        message.reply(
+            {content: "Hi!"}
+        );
+    }
+    if(message.content === 'hey') {
+        message.reply(
+            {content: "Hey!"}
+        );
+    }
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'create') {
+    const url = interaction.options.getString('url');
+
+    if (!url) {
+      return interaction.reply('Please provide a URL.');
+    }
+
+    if (!url.startsWith('http')) {
+      return interaction.reply('Please provide a valid URL.');
+    }
+    //logic using mongodb
+    return interaction.reply(`Creating a new short URL for ${url}`);
+  }
+
+  if (interaction.commandName === 'ping') {
+    return interaction.reply('Pong!');
+  }
+});
